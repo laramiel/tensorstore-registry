2183deec779023f9569e13591517dc03c78b3c87
Laramie Leavitt <lar@google.com>
2025-05-16 18:16:20 -0700

    Update registry after script changes

--- run_tests.sh
+++ run_tests.sh
@@ -6,10 +6,8 @@ function test_repo() {
   # --registry=https://bcr.bazel.build 
   echo "$1"
   cd $1
-  bazelisk test "--registry=${REG:-file:///Users/lar/github/tensorstore-registry}" --registry=https://bcr.bazel.build  --explain=test.txt --verbose_failures //:all
+  bazelisk test "--registry=${REG:-file:///Users/lar/github/tensorstore-registry}" --registry=https://bcr.bazel.build --verbose_failures //:all
 )
 }
 
 for x in tests/*; do test_repo $x; done
-
-# --registry=https://bcr.bazel.build 
